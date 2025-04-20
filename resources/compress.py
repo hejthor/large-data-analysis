dc6fbc2d0f36474e1891c050fa0622ef0a7d55c9
Thor <hejthor@icloud.com>
2025-04-20 08:20:11 +0200

    Update compress.py

--- resources/compress.py
+++ resources/compress.py
@@ -1,6 +1,7 @@
 import dask.dataframe as dd
 import dask
 import os
+import gc
 
 def compress(output, data, memory):
     with dask.config.set(temporary_directory=output):
@@ -14,10 +15,12 @@ def compress(output, data, memory):
                 low_memory=False,
                 blocksize=memory
             )
+            gc.collect()
 
             for col in dataframe.columns:
                 if dataframe[col].nunique().compute() < 1000:
                     dataframe[col] = dataframe[col].astype("category")
+            gc.collect()
 
             print("[PYTHON][compress.py] Saving dataframe to Parquet")
             dataframe.to_parquet(
@@ -25,5 +28,7 @@ def compress(output, data, memory):
                 engine='pyarrow',
                 compression='brotli',
                 use_dictionary=True,
-                write_metadata_file=False
+                write_metadata_file=False,
+                row_group_size=100000
             )
+            gc.collect()
